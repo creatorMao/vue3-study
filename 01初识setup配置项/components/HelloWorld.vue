287211c1da58287e5813adea87b96129c028f95a
Zhiwu Mao <zwmao@outlook.com>
2022-09-02 15:15:44 +0800

    增加ref函数

---
+++ 01初识setup配置项/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>姓名{{ name }}</h1>
+  <h1>姓名{{ age }}</h1>
+  <button @click="showName">展示我</button>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  setup: function () {
+    const name = '毛致武'
+    const age = 18
+
+    function showName() {
+      alert(name)
+    }
+
+    return {
+      name,
+      age,
+      showName
+    }
+  }
+}
+</script>
