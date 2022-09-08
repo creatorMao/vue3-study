388c12c6a6ae427f2febdbb6a749f84086b26ed0
Zhiwu Mao <zwmao@outlook.com>
2022-09-08 14:48:10 +0800

    增加组件通讯代码

---
+++ 07组件通讯/components/Son.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container-son">
+    <div>{{ personInfo.name }}</div>
+  </div>
+</template>
+
+<script>
+import { inject } from 'vue'
+export default {
+  name: 'App',
+  components: {},
+  setup() {
+    const personInfo = inject('personInfo')
+    return {
+      personInfo
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container-son {
+  height: 40px;
+  background-color: yellow;
+}
+</style>
---
+++ src/components/Child.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container-child">
+    <div>{{ personInfo.age }}</div>
+    <Son />
+  </div>
+</template>
+
+<script>
+import Son from '@/components/Son.vue'
+import { inject } from 'vue'
+export default {
+  name: 'App',
+  components: {
+    Son
+  },
+  setup() {
+    const personInfo = inject('personInfo')
+    return {
+      personInfo
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container-child {
+  height: 80px;
+  background-color: blue;
+}
+</style>
